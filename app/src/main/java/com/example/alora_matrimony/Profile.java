b0f63e8af0630ed3e748e16c2f6310adcba598fd
Kish.v17 <kish.v07@gmail.com>
2024-03-30 13:14:18 +0530

    fetchComplete

--- app/src/main/java/com/example/alora_matrimony/Profile.java
+++ app/src/main/java/com/example/alora_matrimony/Profile.java
@@ -2,25 +2,35 @@ package com.example.alora_matrimony;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 import com.example.alora_matrimony.databinding.FragmentProfileBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Profile extends Fragment {
     FragmentProfileBinding b;
+    DatabaseReference dbr;
+    String uid;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         b = FragmentProfileBinding.inflate(inflater, container, false);
         View view = b.getRoot();
+        uid= FirebaseAuth.getInstance().getCurrentUser().getUid();
+        dbr=FirebaseDatabase.getInstance().getReference();
+        readData();
 
         b.ppref.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +74,31 @@ public class Profile extends Fragment {
         b.putInt("btnId",btnId);
         i.putExtras(b);
         startActivity(i);
+    }
+
+    private void readData(){
+        DatabaseReference uRef=dbr.child("Users").child("uid");
+        uRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    UserDetails u=snapshot.getValue(UserDetails.class);
+                    if(u!=null){
+                        b.txtUserName.setText(u.getFirstName()+" "+u.getLastName());
+                        Glide.with(requireContext())
+                                .load(u.getImage()) // Assuming profileImageUrl is the URL in your database
+                                .placeholder(R.drawable.deshboard_profile_circle) // Placeholder image while loading
+                                .error(R.drawable.deshboard_profile_circle) // Error image if loading fails
+                                .into(b.UserProfile);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
